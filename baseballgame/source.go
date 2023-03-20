dd26373baf5e9f4796bd14180f82b351f4449d4b
Vlad Akhpanov <0whiterthanwhite0@gmail.com>
2023-03-20 11:32:25 +0400

    update solution to "baseball game"

--- baseballgame/source.go
+++ baseballgame/source.go
@@ -5,30 +5,56 @@ import (
 )
 
 func calPoints(operations []string) int {
-	sum := 0
-	n := 0
-	stack := make([]int, 0)
+	stack := stack{}
 	for _, v := range operations {
 		switch v {
 		case "C":
-			n--
-			stack = stack[:n]
+			stack.pop()
 		case "D":
-			t := stack[n-1] * 2
-			stack = append(stack, t)
-			n++
+			x := stack.pop()
+			y := x * 2
+			stack.push(x)
+			stack.push(y)
 		case "+":
-			t := stack[n-2] + stack[n-1]
-			stack = append(stack, t)
-			n++
+			x := stack.pop()
+			y := stack.pop()
+			stack.push(y)
+			stack.push(x)
+			stack.push(x + y)
 		default:
 			t, _ := strconv.Atoi(v)
-			stack = append(stack, t)
-			n++
+			stack.push(t)
 		}
 	}
-	for _, v := range stack {
-		sum += v
+	sum := 0
+	for stack.first != nil {
+		sum += stack.pop()
 	}
 	return sum
+}
+
+type node struct {
+	item int
+	next *node
+}
+
+type stack struct {
+	first *node
+	n     int
+}
+
+func (s *stack) push(item int) {
+	oldFirst := s.first
+	s.first = &node{
+		item: item,
+		next: oldFirst,
+	}
+	s.n++
+}
+
+func (s *stack) pop() int {
+	item := s.first.item
+	s.first = s.first.next
+	s.n--
+	return item
 }
