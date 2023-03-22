840c57eca783c04d4b961cc91fefcff89fce09cd
Vlad Akhpanov <0whiterthanwhite0@gmail.com>
2023-03-22 10:50:22 +0400

    update solution to "XOR Operation in an Array"

--- xoroperationinanarray/source.go
+++ xoroperationinanarray/source.go
@@ -1,20 +1,14 @@
 package xoroperationinanarray
 
-import "fmt"
-
 func xorOperation(n, start int) int {
-	nums := make([]int, n)
 	var r int
 	for i := 0; i < n; i++ {
-		nums[i] = start + 2*i
-		if i == 1 {
-			r = nums[i-1] ^ nums[i]
-		} else if i > 1 {
-			r = r ^ nums[i]
+		el := start + 2*i
+		if i == 0 {
+			r = 0 ^ el
 		} else {
-			r = 0 ^ nums[i]
+			r = r ^ el
 		}
 	}
-	fmt.Println(nums, r)
 	return r
 }
