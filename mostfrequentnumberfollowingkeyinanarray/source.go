a9a92e78aeceb908aa6dee5aac1c86df8fa4dc70
Vlad Akhpanov <0whiterthanwhite0@gmail.com>
2023-03-24 11:30:02 +0400

    update solution to "Most Frequent Number Following Key In an Array"

--- mostfrequentnumberfollowingkeyinanarray/source.go
+++ mostfrequentnumberfollowingkeyinanarray/source.go
@@ -4,20 +4,17 @@ import "fmt"
 
 func mostFrequent(nums []int, key int) int {
 	r := make(map[int]int)
+	target := 0
+	count := 0
 	for i := 0; i < len(nums)-1; i++ {
 		if nums[i] == key {
 			r[nums[i+1]]++
-		}
-	}
-
-	count := 0
-	value := 0
-	for k, v := range r {
-		if count < v {
-			count = v
-			value = k
+			if r[nums[i+1]] > count {
+				target = nums[i+1]
+				count = r[nums[i+1]]
+			}
 		}
 	}
 	fmt.Println(r)
-	return value
+	return target
 }
