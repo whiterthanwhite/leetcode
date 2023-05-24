c9b651106848965718a39606cac0756557b10d4d
Vlad Akhpanov <0whiterthanwhite0@gmail.com>
2023-05-24 21:44:43 +0400

    improved solution "Product of Array Except Self"

--- medium/productofarrayexceptself/source.go
+++ medium/productofarrayexceptself/source.go
@@ -2,24 +2,21 @@ package productofarrayexceptself
 
 func productExceptSelf(nums []int) []int {
 	n := len(nums)
-	result := make([]int, n)
 
-	prefixNums := make([]int, n)
-	prefixNums[0] = nums[0]
-	for i := 1; i < n-1; i++ {
-		prefixNums[i] = prefixNums[i-1] * nums[i]
-	}
 	suffixNums := make([]int, n)
 	suffixNums[n-1] = nums[n-1]
 	for i := n - 2; i > 0; i-- {
 		suffixNums[i] = suffixNums[i+1] * nums[i]
 	}
 
+	result := make([]int, n)
+	prefixProd := nums[0]
 	result[0] = suffixNums[1]
-	result[n-1] = prefixNums[n-2]
 	for i := 1; i < n-1; i++ {
-		result[i] = prefixNums[i-1] * suffixNums[i+1]
+		result[i] = prefixProd * suffixNums[i+1]
+		prefixProd *= nums[i]
 	}
+	result[n-1] = prefixProd
 
 	return result
 }
